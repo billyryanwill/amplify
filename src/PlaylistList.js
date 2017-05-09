b041dbd434f5bd2b267170eccee8bb45d2b6dabf
Billy Williams <billy.williams@DISTINCTION-023.distinction.lan>
2017-05-09 17:11:00 +0100

    Demo data added.

--- src/PlaylistList.js
+++ src/PlaylistList.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import {Data as songs} from './data/songs.json';
+console.log(songs);
 const PlaylistList = () => {
     return (
         <div>
            <ul className="playlist--list">
-
+                {songs.map((song, i) => 
+                    <div key={song}>
+                        <a href="#">This is a song title</a>
+                    </div>
+                )}
            </ul>
         </div>
     )
